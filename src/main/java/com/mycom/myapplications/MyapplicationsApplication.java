928a7d43a016b9c77adc352583506bcad128843f
Laxmikanthreddy Koduru <laxmikanthreddy.koduru@pearson.com>
2017-09-05 14:46:47 -0400

    project setup

---
+++ src/main/java/com/mycom/myapplications/MyapplicationsApplication.java
@@ -0,0 +1,12 @@
+package com.mycom.myapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyapplicationsApplication.class, args);
+	}
+}
